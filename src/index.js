183e1823d98f7b1ec04b12575b01cc661972c2fa
Bikram <technology2203@gmail.com>
2024-06-27 01:23:20 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,87 @@
+import "./style.css";
+
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDC964Tn-wWBdGobfARaAmcRzUhf67qLE4",
+  authDomain: "learning-e2d24.firebaseapp.com",
+  projectId: "learning-e2d24",
+  storageBucket: "learning-e2d24.appspot.com",
+  messagingSenderId: "377958973096",
+  appId: "1:377958973096:web:3099321ad39e79c5dd8c9d",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+// init services
+const db = getFirestore();
+
+// Collection Reference
+const colRef = collection(db, "books");
+
+// get collection data
+
+// getDocs(colRef)
+//   .then((snapshot) => {
+//     console.log(snapshot.docs);
+//     let books = [];
+//     snapshot.docs.forEach((doc) => books.push({ ...doc.data(), id: doc.id }));
+//     console.log(books);
+//   })
+//   .catch((err) => console.log(err.message));
+
+const fetchBooks = async () => {
+  try {
+    const snapshot = await getDocs(colRef);
+    // console.log(snapshot.docs);
+    let books = [];
+    snapshot.docs.forEach((doc) => books.push({ ...doc.data(), id: doc.id }));
+    console.log(books);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+fetchBooks();
+
+// Element selection
+const addBook = document.querySelector(".add");
+const deleteBook = document.querySelector(".delete");
+
+// Adding data
+addBook.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  try {
+    await addDoc(colRef, {
+      title: addBook.title.value,
+      author: addBook.author.value,
+    });
+    addBook.reset();
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+// Deleting data
+deleteBook.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const docRef = doc(db, "books", deleteBook.id.value);
+
+  try {
+    await deleteDoc(docRef);
+    deleteBook.reset();
+  } catch (err) {
+    console.log(err.message);
+  }
+});
